21149a08c4f734a23332929ce68cacd424c9206e
Tamer Alsharawneh <tameralsharawneh12@gmail.com>
2025-03-17 00:10:55 +0300

    Learn mouseHover up and down and leave

--- src/test/java/ActionsTest.java
+++ src/test/java/ActionsTest.java
@@ -46,4 +46,63 @@ public class ActionsTest {
         String afterClickText = rightClick.getAttribute("value");
         Assert.assertEquals(afterClickText,"temu");
     }
+
+    /*
+    <input type="button" data-testID="onMouseUp" value="Click on me" onmouseup="this.value='temu'">
+<input type="button" data-testID="onMouseOver" value="Click on me" onmouseover="this.value='temu'">
+<input type="button" data-testID="onMouseLeave" value="Click on me" onmouseleave="this.value='temu'">
+<input type="button" data-testID="onMouseOut" value="Click on me" onmouseout="this.value='temu'">
+     */
+
+    @Test
+    public void onMouseUp() {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("file:\\C:\\Java training projects\\SeleniumIntermidate\\src\\main\\resources\\actions.html");
+        WebElement onMouseOver = driver.findElement(By.cssSelector("[data-testID=\"onMouseOver\"]"));
+        WebElement onMouseLeave = driver.findElement(By.cssSelector("[data-testID=\"onMouseLeave\"]"));
+        Actions actions = new Actions(driver);
+        actions.moveToElement(onMouseOver).moveToElement(onMouseLeave).perform();
+        String afterClickText = onMouseOver.getAttribute("value");
+        Assert.assertEquals(afterClickText,"temu");
+    }
+
+    @Test
+    public void onMouseOver() {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("file:\\C:\\Java training projects\\SeleniumIntermidate\\src\\main\\resources\\actions.html");
+        WebElement rightClick = driver.findElement(By.cssSelector("[data-testID=\"onRightClick\"]"));
+        String beforeClickText = rightClick.getText();
+        Actions actions = new Actions(driver);
+        actions.contextClick(rightClick).perform();
+        String afterClickText = rightClick.getAttribute("value");
+        Assert.assertEquals(afterClickText,"temu");
+    }
+
+    @Test
+    public void onMouseLeave() {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("file:\\C:\\Java training projects\\SeleniumIntermidate\\src\\main\\resources\\actions.html");
+        WebElement rightClick = driver.findElement(By.cssSelector("[data-testID=\"onRightClick\"]"));
+        String beforeClickText = rightClick.getText();
+        Actions actions = new Actions(driver);
+        actions.contextClick(rightClick).perform();
+        String afterClickText = rightClick.getAttribute("value");
+        Assert.assertEquals(afterClickText,"temu");
+    }
+
+    @Test
+    public void onMouseOut() {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("file:\\C:\\Java training projects\\SeleniumIntermidate\\src\\main\\resources\\actions.html");
+        WebElement rightClick = driver.findElement(By.cssSelector("[data-testID=\"onRightClick\"]"));
+        String beforeClickText = rightClick.getText();
+        Actions actions = new Actions(driver);
+        actions.contextClick(rightClick).perform();
+        String afterClickText = rightClick.getAttribute("value");
+        Assert.assertEquals(afterClickText,"temu");
+    }
 }
