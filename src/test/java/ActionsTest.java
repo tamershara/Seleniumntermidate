7a256955115d7ff7ea2d27c7d46a2bb9b11e6b8e
Tamer Alsharawneh <tameralsharawneh12@gmail.com>
2025-03-16 23:59:07 +0300

    Learn double click

--- src/test/java/ActionsTest.java
+++ src/test/java/ActionsTest.java
@@ -2,6 +2,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -14,7 +15,21 @@ public class ActionsTest {
         driver.get("file:\\C:\\Java training projects\\SeleniumIntermidate\\src\\main\\resources\\actions.html");
         WebElement oneClickButton = driver.findElement(By.cssSelector("[data-testID=\"onclick\"]"));
         String beforeClickText = oneClickButton.getText();
-        oneClickButton.click();
+        Actions actions = new Actions(driver);
+        actions.click(oneClickButton).perform();
+        String afterClickText = oneClickButton.getAttribute("value");
+        Assert.assertEquals(afterClickText,"temu");
+    }
+
+    @Test
+    public void onDoubleClick() {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("file:\\C:\\Java training projects\\SeleniumIntermidate\\src\\main\\resources\\actions.html");
+        WebElement oneClickButton = driver.findElement(By.cssSelector("[data-testID=\"onclick\"]"));
+        String beforeClickText = oneClickButton.getText();
+        Actions actions = new Actions(driver);
+        actions.doubleClick(oneClickButton).perform();
         String afterClickText = oneClickButton.getAttribute("value");
         Assert.assertEquals(afterClickText,"temu");
     }
